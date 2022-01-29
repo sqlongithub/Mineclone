9cd05a224fd94233bf31232e56ac09b78921b4ca
Niclas <nniclasss95@gmail.com>
2022-01-29 20:12:09 +0100

    Revert "Implement basic shaders, and draw a fancy triangle!"
    This reverts commit c076e23172b6be2437bd9afd88f714093364771c.

--- Mineclone/src/Mineclone/Application.cpp
+++ Mineclone/src/Mineclone/Application.cpp
@@ -5,7 +5,7 @@
 #include <glad/glad.h>
 
 #include "Application.h"
-#include "Shader.h"
+
 
 namespace Mineclone {
 	Application::Application(const std::string& title, const int width, const int height)
@@ -15,23 +15,19 @@ namespace Mineclone {
 		m_window.setMainLoopCallback([this](GLFWwindow* window) { mainLoop(window); });
 		m_window.setKeyCallback([this](const int key, const int scancode, const int action, const int mods) { onKey(key, scancode, action, mods); });
 
-		float vertices[3*6] = {
-		-0.35f, -0.5f, 0.0f,		1.0f, 0.0f, 0.0f,
-		0.35f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f,
-		0.0f, 0.5f, 0.0f,		0.0f, 0.0f, 1.0f
+		float vertexPositions[3*3] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
 		};
 
 		unsigned int buffer;
 		glGenBuffers(1, &buffer);
 		glBindBuffer(GL_ARRAY_BUFFER, buffer);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, 3*3*sizeof(float), vertexPositions, GL_STATIC_DRAW);
 
 		glEnableVertexAttribArray(0);
-		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 0);
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-
-		Shader shader("src\\Shaders\\vertexShader.glsl", "src\\Shaders\\fragmentShader.glsl");
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
 
 		m_window.setVSync(true);
 		m_startTime = std::chrono::steady_clock::now();
